b55a92b3fd7e9fe5d3de34394160da6a9fec608d
raprap <oloresraprap@gmail.com>
2016-03-18 21:16:39 -0700

    Implemented All Except gmaps because error.

---
+++ app/src/androidTest/java/com/example/android/sunshine/utils/FullTestSuite.java
@@ -0,0 +1,18 @@
+package com.example.android.sunshine.utils;
+
+import junit.framework.TestSuite;
+
+/**
+ * Created by raprap on 3/17/2016.
+ */
+public class FullTestSuite extends TestSuite {
+//
+//    public static Test suite(){
+//        return new TestSuiteBuilder(FullTestSuite.class)
+//                .includeAllPackagesUnderHere().build();
+//    }
+//
+//    public FullTestSuite(){
+//        super();
+//    }
+}
